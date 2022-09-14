93097019447e0972b8cdf354c784631d73610794
makelloss <artemvakhno@gmail.com>
2022-09-14 22:13:38 +0300

    +chek

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
-$a = rand(-100, 100);
-$b = rand(-100, 100);
-$c = rand(-100, 100);
+$a = 0;
+$b = -4;
+$c = -7;
 $d = $b*$b-4*$a*$c;
 echo "D= $d";
-$x1 = -$b+sqrt($d)/2*$a;
-echo "x1=$x1";
-$x2 = -$b-sqrt($d)/2*$a;
-echo "x2=$x2";
+if ($a == 0) {
+    echo "<span style = \" color: red;\">На ноль делить нельзя</span>";
+} else {
+}
+if ($d <0 ) {
+    echo "<span style = \" color: red;\">Коренів немає</span>";
+} elseif ($d == 0) {
+    $x = $b*$b/2*$a;
+    echo "<span style = \" color: green;\">x = $x</span>";
+} else {
+    $x1 = -$b+sqrt($d)/2*$a;
+    $x2 = -$b-sqrt($d)/2*$a; 
+    echo "<span style = \" color: green;\">корни уравнение = $x1, $x2!</span>";
+}
+?>
