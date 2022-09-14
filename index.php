d59deb439098fc94c4f1cd2535d1acc7776b6ba0
makelloss <artemvakhno@gmail.com>
2022-09-14 22:35:58 +0300

    +cheking2,rand

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-$a = 0;
-$b = -4;
-$c = -7;
+$a = -5;
+$b = 4;
+$c = 1;
 $d = $b*$b-4*$a*$c;
 echo "D= $d";
-if ($a == 0) {
-    echo "<span style = \" color: red;\">На ноль делить нельзя</span>";
-} else {
-}
 if ($d <0 ) {
     echo "<span style = \" color: red;\">Коренів немає</span>";
 } elseif ($d == 0) {
     $x = $b*$b/2*$a;
     echo "<span style = \" color: green;\">x = $x</span>";
-} else {
+} 
+elseif ($a == 0) {
+    echo "<span style = \" color: red;\">На ноль делить нельзя</span>";
+}else {
     $x1 = -$b+sqrt($d)/2*$a;
     $x2 = -$b-sqrt($d)/2*$a; 
     echo "<span style = \" color: green;\">корни уравнение = $x1, $x2!</span>";
